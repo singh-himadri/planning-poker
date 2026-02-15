1b4127fb4da1aa3b569078354ba3cd083a68ac23
singh-himadri <singhhimadri727@gmail.com>
2026-02-15 20:23:13 +0530

    Add custom server for Render deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  output: 'standalone',
   // Ensure we don't have issues with strict mode for socket double connect
   reactStrictMode: false,
 };
